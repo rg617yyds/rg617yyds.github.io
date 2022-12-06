b306699c04209cd8eb5f1a813ca478c297b376ab
rg617yyds <111485998+rg617yyds@users.noreply.github.com>
2022-12-06 15:41:35 +0000

    🎉 INT COMMIT: 12.7 Updated By Github Actions

--- js/custom/cheesy_pick-up_lines.js
+++ js/custom/cheesy_pick-up_lines.js
@@ -1,16 +1,1 @@
-function cheesy_pick_up_lines() {
-    let html = "";
-    for (let i = 0; i < 3; i++) {
-      fetch("https://api.vvhan.com/api/love")
-        .then((data) => data.text())
-        .then((data) => {
-          html += "<li>" + data + "</li>";
-          document.querySelector("#cheesy_pick-up_lines-container").innerHTML =
-            html;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  }
-  
+function cheesy_pick_up_lines(){let e="";for(let n=0;n<3;n++)fetch("https://api.vvhan.com/api/love").then((e=>e.text())).then((n=>{e+="<li>"+n+"</li>",document.querySelector("#cheesy_pick-up_lines-container").innerHTML=e})).catch((function(e){console.log(e)}))}
